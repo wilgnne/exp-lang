7167865adfe51ee28de3c65cd04e1388d2686541
Wilgnne Khawan <wilgnne.kba@gmail.com>
2021-05-18 18:13:59 +0000

    feat: Test read_str

--- test/15-string/index.spec.js
+++ test/15-string/index.spec.js
@@ -5,14 +5,18 @@ describe('String', () => {
   let out
 
   beforeAll(() => {
-    out = build_run(path.resolve(__dirname, 'string.exp'))
+    out = build_run(path.resolve(__dirname, 'string.exp'), 'Hello World\n')
   });
 
   test("This is just a test!", () => {
     expect(out[0]).toBe("This is just a test!");
   })
 
+  test("read_str", () => {
+    expect(out[1]).toBe("Hello World");
+  })
+
   test("No more outputs", () => {
-    expect(out[1]).toBe("");
+    expect(out[2]).toBe("");
   })
 })
